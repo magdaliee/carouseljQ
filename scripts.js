88d94680f73cd014176020c74fbaf7304bc80900
HP-450G2\Admin <lameklena@gmail.com>
2017-04-03 21:36:58 +0200

    m

--- scripts.js
+++ scripts.js
@@ -1,9 +1,34 @@
 $(function(){
 	var carouselList = $("#carousel ul");
-	var changeslide = setInterval(function() {
-		$ (carouselList).animate({marginLeft:-400},1000,function() {
-			$(this).find("li:last").after($(this).find("li:first"));
-			$(this).css({marginLeft:0});
-		})
-	},3000);
+	//setInterval(changeSlide, 3000);
+	function changeSlideNext() {
+		carouselList.animate({'marginLeft': -500}, 1000, moveFirstSlide);
+	};
+	function moveFirstSlide () {
+		var firstItem = carouselList.find("li:first");
+		var lastItem = carouselList.find("li:last");
+		lastItem.after(firstItem);
+		carouselList.css({marginLeft:0});
+	};
+
+	function moveLastSlide () {
+		var firstItem = carouselList.find("li:first");
+		var lastItem = carouselList.find("li:last");
+		firstItem.before(lastItem);
+		carouselList.css({marginRight:-500});
+	};
+
+	function changeSlidePrev() {
+		carouselList.animate({'marginRight':0}, 1000, moveLastSlide);
+	};
+
+	var nextSlide = $("#right");
+	nextSlide.click(function () {
+		changeSlideNext();
+	});
+
+	var prevSlide = $("#left");
+	prevSlide.click(function () {
+		changeSlidePrev();
+});
 });
